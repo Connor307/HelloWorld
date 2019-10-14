4467bb94610fe9a463dc0f5e79d2f5a68f55e764
Connor L <44709700+Connor307@users.noreply.github.com>
2019-10-14 11:28:11 -0700

    Adding a button press and light flash

--- main.ts
+++ main.ts
@@ -1,1 +1,5 @@
-
+brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
+    brick.setStatusLight(StatusLight.GreenFlash);
+    brick.showString("Hello World", 1);
+    brick.showString("You are living in a simulation", 2);
+})
