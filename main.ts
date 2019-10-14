93b445f594f50ae7cfc8436787ac83cecde772b1
Connor L <44709700+Connor307@users.noreply.github.com>
2019-10-14 11:31:04 -0700

    Added a new line

--- main.ts
+++ main.ts
@@ -2,4 +2,5 @@ brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
     brick.setStatusLight(StatusLight.GreenFlash);
     brick.showString("Hello World", 1);
     brick.showString("You are living in a simulation", 2);
-})
+    brick.showString("You need to get out right now", 3);
+});
